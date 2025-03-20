d4e4d71420cb2c096d358f2b625f6ba0522567a1
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-20 12:12:52 -0300

    fix: atualiza criarUsuarioEntityUtil para definir email e role do usuário

--- backend/src/test/java/br/com/gestorfinanceiro/TestDataUtil.java
+++ backend/src/test/java/br/com/gestorfinanceiro/TestDataUtil.java
@@ -1,12 +1,16 @@
 package br.com.gestorfinanceiro;
 
 import br.com.gestorfinanceiro.models.UserEntity;
+import br.com.gestorfinanceiro.models.enums.Roles;
 
 public class TestDataUtil {
     public static UserEntity criarUsuarioEntityUtil(String nome) {
         UserEntity user = new UserEntity();
-        user.setUsername(nome + "@gmail.com");
+        user.setUsername(nome);
+        user.setEmail(nome + "@gmail.com");
         user.setPassword("123456");
+        user.setRole(Roles.USER);
+
         return user;
     }
 }
